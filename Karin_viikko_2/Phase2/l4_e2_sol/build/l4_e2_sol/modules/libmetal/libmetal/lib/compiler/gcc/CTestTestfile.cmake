c69a3ab48cca760b9d2b002fdf62d7b07ad70164
Svveri <severi.ronk@gmail.com>
2025-11-04 19:30:05 +0200

    Lisätty phase2 koodit eli valmis nordicin lesson4e2 koodi, jossa on bluetooth lähetykselle yms kaikki tarpeellinen, tämäkin toimi oikein hyvin mikrokontrollerille flashattyna sekä tätä käytettiin phase1 ja phase2 yhdistyksessä.

---
+++ Karin_viikko_2/Phase2/l4_e2_sol/build/l4_e2_sol/modules/libmetal/libmetal/lib/compiler/gcc/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/ncs/v3.1.1/modules/hal/libmetal/libmetal/lib/compiler/gcc
+# Build directory: C:/Tietoliikenne_project/Phase2/l4_e2_sol/build/l4_e2_sol/modules/libmetal/libmetal/lib/compiler/gcc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
